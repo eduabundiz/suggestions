a545959f81a4a4f8dcc6bd44dfa1c58cf4329e63
EduardoAbundiz <3duabundiz@gmail.com>
2023-09-14 15:29:47 -0600

    Adding swagger

--- src/main/java/com/gendra/suggestion/controller/SuggestionController.java
+++ src/main/java/com/gendra/suggestion/controller/SuggestionController.java
@@ -5,12 +5,14 @@ import com.gendra.suggestion.Entity.SuggestionResponse;
 import com.gendra.suggestion.service.SuggestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/suggestions")
 public class SuggestionController {
     private final SuggestionService suggestionService;
 
@@ -18,7 +20,7 @@ public class SuggestionController {
     public SuggestionController(SuggestionService suggestionService) {
         this.suggestionService = suggestionService;
     }
-    @GetMapping("/suggestions")
+    @GetMapping("")
     public SuggestionResponse getSuggestions(
             @RequestParam("q") String query,
             @RequestParam(value = "latitude", required = false) Double latitude,
