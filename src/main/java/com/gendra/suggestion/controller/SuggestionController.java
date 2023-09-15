2a21b674b8c434f0af11b47638cecaa6ccdff245
EduardoAbundiz <3duabundiz@gmail.com>
2023-09-14 22:32:03 -0600

    Adding parameter for swagger

--- src/main/java/com/gendra/suggestion/controller/SuggestionController.java
+++ src/main/java/com/gendra/suggestion/controller/SuggestionController.java
@@ -4,6 +4,8 @@ import com.gendra.suggestion.entity.Suggestion;
 import com.gendra.suggestion.entity.SuggestionResponse;
 import com.gendra.suggestion.service.SuggestionService;
 import java.util.List;
+
+import io.swagger.v3.oas.annotations.Parameter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,9 +22,9 @@ public class SuggestionController {
 
   @GetMapping("/suggestions")
   public SuggestionResponse getSuggestions(
-      @RequestParam("q") String query,
-      @RequestParam(value = "latitude", required = false) Double latitude,
-      @RequestParam(value = "longitude", required = false) Double longitude) {
+      @Parameter(description = "query", example = "london") @RequestParam("q") String query,
+      @Parameter(description = "Latitude", example = "43.70011") @RequestParam(value = "latitude", required = false) Double latitude,
+      @Parameter(description = "Latitude", example = "-79.4163") @RequestParam(value = "longitude", required = false) Double longitude) {
     List<Suggestion> suggestions = suggestionService.getSuggestions(query, latitude, longitude);
     return new SuggestionResponse(suggestions);
   }
