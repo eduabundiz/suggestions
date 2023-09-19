e11b1261b4ae41c7f1e3718eafd8eeccffc6bf20
EduardoAbundiz <3duabundiz@gmail.com>
2023-09-18 18:43:41 -0600

    JavaDocs

---
+++ package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.gendra.suggestion"},{"l":"com.gendra.suggestion.config"},{"l":"com.gendra.suggestion.controller"},{"l":"com.gendra.suggestion.entity"},{"l":"com.gendra.suggestion.repository"},{"l":"com.gendra.suggestion.service"},{"l":"com.gendra.suggestion.util"}];updateSearchResults();
